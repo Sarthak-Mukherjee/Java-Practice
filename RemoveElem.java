462f49550c2913835ce605d2881605ca0c901952
Sarthak-Mukherjee <sarthakmukherje2003@gmal.com>
2025-09-18 16:22:16 +0530

    Added RemoveELem.java

---
+++ RemoveElem.java
@@ -0,0 +1,16 @@
+public class RemoveElem {
+    public static void main(String[] args) {
+        int nums[] ={3,2,2,3};
+        int val = 3, j =0;
+        for (int i=0; i<nums.length; i++){
+            if(nums[i] != val){
+                nums[j++] = nums[i];
+            }
+        }for(int i = 0 ; i<j; i++){
+        System.out.println(nums[j]);
+        
+    }
+    System.out.println(j);
+    
+}
+}
