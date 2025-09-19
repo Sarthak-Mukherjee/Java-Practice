3bf3d503a61d53973943ed0cbe31a701e13bd1ad
Sarthak-Mukherjee <sarthakmukherje2003@gmal.com>
2025-09-19 20:43:35 +0530

    Added firstandlastposi

---
+++ FirstAndLastPosi.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+public class FirstAndLastPosi {
+    public static void main(String[] args) {
+        int[] nums = {5, 7, 7, 8, 8, 10};
+        int target = 8;
+        int first = -1, last = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                if (first == -1) first = i;
+                last = i;
+            }
+        }
+
+        int[] result = {first, last};
+        System.out.println(Arrays.toString(result));
+    }
+}
