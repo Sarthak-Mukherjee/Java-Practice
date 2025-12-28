c4dbde2574162ab9db906748ba8b222a286a6974
Sarthak-Mukherjee <sarthakmukherje2003@gmal.com>
2025-12-28 15:49:16 +0530

    .gitignore

--- LeetCode/Test.java
+++ LeetCode/Test.java
@@ -1,20 +1,29 @@
 import java.util.*;
 class Test{
-    public List<List<String>> groupAnagrams(String[] strs){
-        Map<String, List<String>> map = new HashMap<>();
-        for(String s : strs){
-            char[] sorted = s.toCharArray();
-            Arrays.sort(sorted);
-        
-            String key = new String(sorted);
-            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+    static int longestConsecutive(int[] nums){
+        Set<Integer> numSet = new TreeSet<>();
+        for(int num : nums){
+            numSet.add(num);
         }
 
-        return new ArrayList<>(map.values());
+        int longest_streak = 0;
+        for(int num : numSet){
+            if(!numSet.contains(num-1)){
+                int current_num = num;
+                int current_streak = 1;
+
+                while(numSet.contains(current_num+1)){
+                    current_num = current_num + 1;
+                    current_streak = current_streak + 1;
+                }
+                longest_streak = Math.max(longest_streak, current_streak);
+            }
+        }
+        return longest_streak;
     }
-    public static void main(String[] args){
-        Test test = new Test();
-        String[] strs = {"eat","tea","tan","ate","nat","bat"};
-        List<List<String>> result = test.groupAnagrams(strs);
-        System.out.println(result);
-    }}
+     public static void main(String[] args) {
+        int[] nums = {100, 4, 200, 1, 3, 2};
+        int result = longestConsecutive(nums);
+        System.out.println(result); // Output: 4
+    }
+}
