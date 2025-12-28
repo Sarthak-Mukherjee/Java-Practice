5bd2067e61982679f9d251e77a3f5ef6fda662cc
Sarthak-Mukherjee <sarthakmukherje2003@gmal.com>
2025-12-28 15:44:31 +0530

    updated DP files

---
+++ DP/CoinChange.java
@@ -0,0 +1,8 @@
+public class CoinChange {
+    public int coinChange(int[] coins, int amount){
+        int[] dp = new int[amount + 1];
+
+        if(amount == 0) dp[0] = 0;
+        
+    }
+}
---
+++ DP/CountingStairs.py
@@ -0,0 +1,17 @@
+class CountingStairs:
+    def count(self, n:int) -> int:
+         
+        if n <=2 : return n
+
+        dp = [0] * (n+1)
+        dp[1] = 1
+        dp[2] = 2
+
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n]
+    
+if __name__ == "__main__":
+    cnt = CountingStairs()
+    print(cnt.count(10))
